fc2dc4e7626c653a05ef76a6c09671fcb894349b
Jesus Imery <yisas_0929@hotmail.com>
2018-02-15 19:07:35 -0500

    #4 Networking test following Unity tutorial

---
+++ Assets/Scripts/Test Scripts/TestPlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestPlayerMove : NetworkBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!isLocalPlayer)
+            return;
+
+        float x = Input.GetAxis("Horizontal 1");
+        float z = Input.GetAxis("Vertical 1");
+        transform.Translate(x, 0, z);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
