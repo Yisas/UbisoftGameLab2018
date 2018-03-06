23ebdea75aef74edcfb12013c6dcbfcd529a152e
Sabz <sabrina.d.mello@hotmail.com>
2018-03-06 17:29:58 -0500

    #50 Button Prompt Triggers when the player is near presure plate (prompt appears on plate). && Pushable Object Prompts on the head of player 1 (Need to make is dynamic for both players)

---
+++ Assets/buttonPrompt.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonPrompt : MonoBehaviour {
+
+    [SerializeField]
+    Canvas buttonPromptCanvas;
+
+
+    void Start()
+    {
+        buttonPromptCanvas.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            TurnOnPrompt();
+        }
+    }
+
+    private void TurnOnPrompt()
+    {
+        buttonPromptCanvas.enabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            TurnOffPrompt();
+        }
+    }
+
+    private void TurnOffPrompt()
+    {
+        buttonPromptCanvas.enabled = false;
+    }
+}
