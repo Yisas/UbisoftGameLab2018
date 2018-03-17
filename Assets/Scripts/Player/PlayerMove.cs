0f3b8f812797be5e23f6bc1c7e35c87f57612fba
Vladimir de la Cruz <vladimirdlc@gmail.com>
2018-03-17 02:32:01 -0400

    Removing unnecessary Log

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -95,7 +95,6 @@ public class PlayerMove : MonoBehaviour
     //get state of player, values and input
     void Update()
     {
-        Debug.Log(GetComponent<Rigidbody>().velocity);
         //handle jumping
         JumpCalculations();
         //adjust movement values if we're in the air or on the ground
