a6f38cb3bacbf85a162c45ec717a41c20ed4eb27
Jesus Imery <yisas_0929@hotmail.com>
2018-03-20 10:37:46 -0400

    Custom network manager doesn't try to mess with mainmenu if scene is loaded directly while developing

--- Assets/Scripts/Networking/CustomNetworkManager.cs
+++ Assets/Scripts/Networking/CustomNetworkManager.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class CustomNetworkManager : NetworkManager {
-
+public class CustomNetworkManager : NetworkManager
+{
     public override void OnServerConnect(NetworkConnection conn)
     {
         base.OnServerConnect(conn);
+
+#if UNITY_EDITOR
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex != 0)
+            return;
+#endif
+
         GetComponent<NetworkManagerHUD>().showGUI = false;
         GameObject.FindGameObjectWithTag("MenuUI").GetComponent<Canvas>().enabled = true;
     }
@@ -15,6 +21,12 @@ public class CustomNetworkManager : NetworkManager {
     public override void OnClientConnect(NetworkConnection conn)
     {
         base.OnClientConnect(conn);
+
+#if UNITY_EDITOR
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex != 0)
+            return;
+#endif
+
         GetComponent<NetworkManagerHUD>().showGUI = false;
     }
 }
