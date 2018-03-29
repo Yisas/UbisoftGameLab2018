afb85e87c9320deec4435094c7a6959f227597fc
Jesus Imery <yisas_0929@hotmail.com>
2018-03-29 02:09:37 -0400

    #131 Added client starter to all levels + conditional compilation

--- Assets/Scripts/Networking/NetworkClientSceneReady.cs
+++ Assets/Scripts/Networking/NetworkClientSceneReady.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿#define LAN_CONNECTION_FIX
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -8,9 +10,11 @@ public class NetworkClientSceneReady : NetworkBehaviour
     // Use this for initialization
     void Start()
     {
+#if LAN_CONNECTION_FIX
         if (!isServer)
         {
             ClientScene.AddPlayer(2);
         }
+#endif
     }
 }
