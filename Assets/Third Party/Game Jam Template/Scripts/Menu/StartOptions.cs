fa88a5549d3e59f013d35c47e454c60f6ebbdccf
Jesus Imery <yisas_0929@hotmail.com>
2018-04-01 16:55:12 -0400

    Lvl 7 loading bug fix

--- Assets/Third Party/Game Jam Template/Scripts/Menu/StartOptions.cs
+++ Assets/Third Party/Game Jam Template/Scripts/Menu/StartOptions.cs
@@ -5,34 +5,38 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
-public class StartOptions : NetworkBehaviour {
+public class StartOptions : NetworkBehaviour
+{
+    public int lastLevelIndex;
     public MenuSettings menuSettingsData;
-	public int sceneToStart = 1;										//Index number in build settings of scene to load if changeScenes is true
-	public bool changeScenes;											//If true, load a new scene when Start is pressed, if false, fade out UI and continue in single scene
-	public bool changeMusicOnStart;										//Choose whether to continue playing menu music or start a new music clip
+    public int sceneToStart = 1;                                        //Index number in build settings of scene to load if changeScenes is true
+    public bool changeScenes;                                           //If true, load a new scene when Start is pressed, if false, fade out UI and continue in single scene
+    public bool changeMusicOnStart;										//Choose whether to continue playing menu music or start a new music clip
     public CanvasGroup fadeOutImageCanvasGroup;                         //Canvas group used to fade alpha of image which fades in before changing scenes
     public Image fadeImage;                                             //Reference to image used to fade out before changing scenes
     public float fadeOutTime = 0.6f;                                           // Amount of time for the screen to fade out
     public float fullFadeTime = 0.5f;                                          // Amount of time for screen to stay dark
     public float fadeInTime = 1f;                                             // Amount of time for screen to fade back in
 
-    [HideInInspector] public bool inMainMenu = true;					//If true, pause button disabled in main menu (Cancel in input manager, default escape key)
-	[HideInInspector] public AnimationClip fadeAlphaAnimationClip;		//Animation clip fading out UI elements alpha
+    [HideInInspector]
+    public bool inMainMenu = true;                  //If true, pause button disabled in main menu (Cancel in input manager, default escape key)
+    [HideInInspector]
+    public AnimationClip fadeAlphaAnimationClip;        //Animation clip fading out UI elements alpha
 
 
-	private PlayMusic playMusic;										//Reference to PlayMusic script
-	private float fastFadeIn = .01f;									//Very short fade time (10 milliseconds) to start playing music immediately without a click/glitch
-	private ShowPanels showPanels;										//Reference to ShowPanels script on UI GameObject, to show and hide panels
+    private PlayMusic playMusic;                                        //Reference to PlayMusic script
+    private float fastFadeIn = .01f;                                    //Very short fade time (10 milliseconds) to start playing music immediately without a click/glitch
+    private ShowPanels showPanels;										//Reference to ShowPanels script on UI GameObject, to show and hide panels
     private CanvasGroup menuCanvasGroup;
 
 
     void Awake()
-	{
-		//Get a reference to ShowPanels attached to UI object
-		showPanels = GetComponent<ShowPanels> ();
+    {
+        //Get a reference to ShowPanels attached to UI object
+        showPanels = GetComponent<ShowPanels>();
 
-		//Get a reference to PlayMusic attached to UI object
-		playMusic = GetComponent<PlayMusic> ();
+        //Get a reference to PlayMusic attached to UI object
+        playMusic = GetComponent<PlayMusic>();
 
         //Get a reference to the CanvasGroup attached to the main menu so that we can fade it's alpha
         menuCanvasGroup = GetComponent<CanvasGroup>();
@@ -48,7 +52,7 @@ public class StartOptions : NetworkBehaviour {
             NextScene();
         }
 
-        if(Input.GetButtonDown("Previous Level"))
+        if (Input.GetButtonDown("Previous Level"))
         {
             Debug.Log("Skipping to previous level");
             PreviousScene();
@@ -57,16 +61,16 @@ public class StartOptions : NetworkBehaviour {
 
 
     public void StartButtonClicked()
-	{
+    {
         Cursor.visible = false;
 
         AkSoundEngine.PostEvent("ui_validate", gameObject);
         //If changeMusicOnStart is true, fade out volume of music group of AudioMixer by calling FadeDown function of PlayMusic
         //To change fade time, change length of animation "FadeToColor"
-        if (menuSettingsData.musicLoopToChangeTo != null) 
-		{
-			playMusic.FadeDown(menuSettingsData.menuFadeTime);
-		}
+        if (menuSettingsData.musicLoopToChangeTo != null)
+        {
+            playMusic.FadeDown(menuSettingsData.menuFadeTime);
+        }
 
         //If changeScenes is true, start fading and change scenes halfway through animation when screen is blocked by FadeImage
         if (menuSettingsData.nextSceneIndex != 0)
@@ -79,20 +83,21 @@ public class StartOptions : NetworkBehaviour {
                 RpcStartCameraFade();
                 NetworkedSceneChange();
             }
-        } 
+        }
 
-		//If changeScenes is false, call StartGameInScene
-		else 
-		{
-			//Call the StartGameInScene function to start game without loading a new scene.
-			StartGameInScene();
-		}
+        //If changeScenes is false, call StartGameInScene
+        else
+        {
+            //Call the StartGameInScene function to start game without loading a new scene.
+            StartGameInScene();
+        }
 
-	}
+    }
 
     [ClientRpc]
     public void RpcStartCameraFade()
     {
+        Cursor.visible = false;
         StartCameraFade();
     }
 
@@ -106,10 +111,19 @@ public class StartOptions : NetworkBehaviour {
     private void NetworkedSceneChange()
     {
         NetworkServer.SetAllClientsNotReady();
-        string path = SceneUtility.GetScenePathByBuildIndex(sceneToStart);
-        string sceneName = path.Substring(0, path.Length - 6).Substring(path.LastIndexOf('/') + 1);
-        playSceneAudio(sceneName);
-        NetworkManager.singleton.ServerChangeScene(sceneName);
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+
+        if (SceneManager.GetActiveScene().buildIndex < lastLevelIndex)
+        {
+            string path = SceneUtility.GetScenePathByBuildIndex(sceneToStart);
+            string sceneName = path.Substring(0, path.Length - 6).Substring(path.LastIndexOf('/') + 1);
+            playSceneAudio(sceneName);
+            NetworkManager.singleton.ServerChangeScene(sceneName);
+        }
+        else
+        {
+            RestartGame();
+        }
     }
 
     private void playSceneAudio(string sceneName)
@@ -203,48 +217,48 @@ public class StartOptions : NetworkBehaviour {
     //Once the level has loaded, check if we want to call PlayLevelMusic
     void SceneWasLoaded(Scene scene, LoadSceneMode mode)
     {
-		//if changeMusicOnStart is true, call the PlayLevelMusic function of playMusic
-		if (menuSettingsData.musicLoopToChangeTo != null)
-		{
-			playMusic.PlayLevelMusic ();
-		}
+        //if changeMusicOnStart is true, call the PlayLevelMusic function of playMusic
+        if (menuSettingsData.musicLoopToChangeTo != null)
+        {
+            playMusic.PlayLevelMusic();
+        }
 
-        if(scene.buildIndex > 0)
+        if (scene.buildIndex > 0)
             StartCoroutine(FadeCanvasGroupAlpha(1f, 0f, fadeOutImageCanvasGroup));
     }
 
     public void LoadDelayed()
-	{
-		//Pause button now works if escape is pressed since we are no longer in Main menu.
-		inMainMenu = false;
+    {
+        //Pause button now works if escape is pressed since we are no longer in Main menu.
+        inMainMenu = false;
 
-		//Hide the main menu UI element
-		showPanels.HideMenu ();
+        //Hide the main menu UI element
+        showPanels.HideMenu();
 
-		//Load the selected scene, by scene index number in build settings
-		SceneManager.LoadScene (sceneToStart);
-	}
+        //Load the selected scene, by scene index number in build settings
+        SceneManager.LoadScene(sceneToStart);
+    }
 
-	public void HideDelayed()
-	{
-		//Hide the main menu UI element after fading out menu for start game in scene
-		showPanels.HideMenu();
-	}
+    public void HideDelayed()
+    {
+        //Hide the main menu UI element after fading out menu for start game in scene
+        showPanels.HideMenu();
+    }
 
-	public void StartGameInScene()
-	{
-		//Pause button now works if escape is pressed since we are no longer in Main menu.
-		inMainMenu = false;
+    public void StartGameInScene()
+    {
+        //Pause button now works if escape is pressed since we are no longer in Main menu.
+        inMainMenu = false;
 
-		//If there is a second music clip in MenuSettings, fade out volume of music group of AudioMixer by calling FadeDown function of PlayMusic 
-		if (menuSettingsData.musicLoopToChangeTo != null) 
-		{
-			//Wait until game has started, then play new music
-			Invoke ("PlayNewMusic", menuSettingsData.menuFadeTime);
-		}
+        //If there is a second music clip in MenuSettings, fade out volume of music group of AudioMixer by calling FadeDown function of PlayMusic 
+        if (menuSettingsData.musicLoopToChangeTo != null)
+        {
+            //Wait until game has started, then play new music
+            Invoke("PlayNewMusic", menuSettingsData.menuFadeTime);
+        }
 
-        StartCoroutine(FadeCanvasGroupAlpha(1f,0f, menuCanvasGroup));
-	}
+        StartCoroutine(FadeCanvasGroupAlpha(1f, 0f, menuCanvasGroup));
+    }
 
     public IEnumerator FadeCanvasGroupAlpha(float startAlpha, float endAlpha, CanvasGroup canvasGroupToFadeAlpha, bool destroy = false)
     {
@@ -272,7 +286,7 @@ public class StartOptions : NetworkBehaviour {
         StartCoroutine(FadeOutThenInRoutine(0f, 1f, fadeOutImageCanvasGroup, player));
     }
 
-    public IEnumerator FadeOutThenInRoutine(float startAlpha, float endAlpha, CanvasGroup canvasGroupToFadeAlpha, PlayerMove player, bool destroy = false )
+    public IEnumerator FadeOutThenInRoutine(float startAlpha, float endAlpha, CanvasGroup canvasGroupToFadeAlpha, PlayerMove player, bool destroy = false)
     {
         // Stop player movement
         player.canMove = false;
@@ -318,12 +332,12 @@ public class StartOptions : NetworkBehaviour {
 
 
     public void PlayNewMusic()
-	{
-		//Fade up music nearly instantly without a click 
-		playMusic.FadeUp (fastFadeIn);
-		//Play second music clip from MenuSettings
-		playMusic.PlaySelectedMusic (menuSettingsData.musicLoopToChangeTo);
-	}
+    {
+        //Fade up music nearly instantly without a click 
+        playMusic.FadeUp(fastFadeIn);
+        //Play second music clip from MenuSettings
+        playMusic.PlaySelectedMusic(menuSettingsData.musicLoopToChangeTo);
+    }
 
     [ClientRpc]
     private void RpcActivateCanvas()
